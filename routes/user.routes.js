58f56114711be8e1588cd36069c53efc246eefea
Shumbusho David <75299674+Shumbusho43@users.noreply.github.com>
2023-11-26 17:54:45 +0200

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createUser, login } = require('../controller/User.controller');
+const { createUser, login } = require('../controller/user.controller');
 const router = express.Router();
 router.post('/api/v1/account/signup', createUser);
 router.post("/api/v1/account/login", login)
