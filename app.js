194db84ff94be04c641a52f33d11b112feec008e
Shumbusho43 <davidshumbusho10@gmail.com>
2026-02-25 12:53:02 +0200

    testing deployments when pipelines fail

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { dbConnection } = require("./models/db");
 const { UserRoutes } = require("./routes/user.routes");
 const { Project } = require("./routes/project.routes");
 const { contactMe } = require("./routes/contactMe.route");
-const { Resume } = require("./routes/resume.routes");
+const { Resume } = require("./routes/resume.route");
 dotenv.config()
 const port=process.env.PORT || 3000
 const app=express()
@@ -25,7 +25,7 @@ app.use("/documentation", swaggerUi.serve, swaggerUi.setup(swaggerFile, false, {
     docExpansion: "none"
 }))
 app.get("/testing-pipelines",(req,res)=>{;
-    res.json({message:"Testing pipelines working correctly!"})
+    res.json({message:"Heyyy, I am testing pipelines"})
 })
 
 app.listen(port,()=>{
